bfe2f587dfffa1111458d9fb01dbdcdbd830fefe
Chandra Mohan Soni <chso@microsoft.com>
2020-12-17 11:52:56 -0800

    adding parameters constants

--- practice/pytorch/money.py
+++ practice/pytorch/money.py
@@ -1,6 +1,27 @@
 import pandas as ps
 
-train = ps.read_csv('data/MSFT.csv')
+train = ps.read_csv('data/newmsft.csv')
 
 train.head()
+
+neuronCount = [31, 23, 10, 8, 5, 3,1]
+
 print(train)
+
+# parameters
+# date of quarter
+# time
+# previous tick - lowlow - lowhigh - highlow - highhigh
+# tick before that - lowlow - lowhigh - highlow - highhigh
+# tick before that - lowlow - lowhigh - highlow - highhigh
+# tick before that - lowlow - lowhigh - highlow - highhigh
+# tick before that - lowlow - lowhigh - highlow - highhigh
+# tick before that - lowlow - lowhigh - highlow - highhigh
+# tick before that - lowlow - lowhigh - highlow - highhigh
+# constant
+
+# w1 = 23 x 31 b1 = 23 x 1
+# w2 = 10 x 23 b2 = 10 x 1
+# w3 = 8 x 10 b3 = 8 x 1
+# w4 = 5 x 8 b4 = 5 x 1
+# w5 = 3 x 5 b5 = 3 x 1
