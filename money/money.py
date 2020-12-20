5f8c54f7eac58362875601dc07e6bb5ad761e1c6
chandra mohan soni <chandramohan.soni@gmail.com>
2020-12-19 18:56:32 -0800

    Moving forward away as a separate function

--- money/money.py
+++ money/money.py
@@ -31,38 +31,37 @@ x[:, 0] = np.ones(size)
 x[:, 1] = np.ones(size)
 x[:, 2] = np.arange(size)
 x = x.T
-output = np.random.uniform(lowvalue, highvalue, (size, 1))
+y = np.random.uniform(lowvalue, highvalue, (size, 1))
 
-# let's make w1 and b1 now to get to second layer
-# going with 64 neurons in layer 2
+n2Count = 250
+w3 = np.random.randn(n2Count, nparam)
+b3 = np.random.uniform(-1, 1 ,(n2Count, 1))
 n1Count = 64
 w1 = np.random.randn(n1Count, nparam) * 0.01
 b1 = np.random.uniform(-1, 1, (n1Count, 1))
 
-# forward function
-
-a1 = linear.combination(w1, x, b1)
-
-# ready for layer 2
-a1sq = np.square(a1)
-sqinput = np.concatenate((a1, a1sq, np.ones((1, size))), 0)
-
-n2Count = 250
 w2 = np.random.randn(n2Count, n1Count * 2 + 1) * 0.01
 b2 = np.random.uniform(-1, 1 ,(n2Count, 1))
 
-lineara1 = linear.combination(w2, sqinput, b2)
 
-a2 = bernauli.sigmoid(lineara1)
+def forward(x, w1, b1, w2, b2, w3, b3, size):
+    # forward function
+    a1 = linear.combination(w1, x, b1)
+    # ready for layer 2
+    a1sq = np.square(a1)
+    sqinput = np.concatenate((a1, a1sq, np.ones((1, size))), 0)
+    lineara1 = linear.combination(w2, sqinput, b2)
+    a2 = bernauli.sigmoid(lineara1)
+    # a2 is now activators for patterns
+    patterns = linear.combination(w3, x, b3)
+    patternImpact = patterns * a2
+    return np.sum(patternImpact, 0)
+
+def backward():
+    return 1
 
 
-# a2 is now activators for patterns
+# let's make w1 and b1 now to get to second layer
+# going with 64 neurons in layer 2
 
-w3 = np.random.randn(n2Count, nparam) * 0.01
-b3 = np.random.uniform(-1, 1 ,(n2Count, 1))
-patterns = linear.combination(w3, x, b3)
-
-patternImpact = patterns * a2
-output = np.sum(patternImpact, 0)
-
-print(output)
+output = forward(x, w1, b1, w2, b2, w3, b3, size)
